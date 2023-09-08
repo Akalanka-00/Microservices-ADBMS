e59cf85b298f1d47f2f66cd40991fe3219ac9c92
Randima Mallawa <mallawamrrp.20@uom.lk>
2023-09-09 01:55:34 +0530

    test

---
+++ src/main/java/com/microservice/order_placement/OrderPlacementApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.order_placement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPlacementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderPlacementApplication.class, args);
+	}
+
+}
